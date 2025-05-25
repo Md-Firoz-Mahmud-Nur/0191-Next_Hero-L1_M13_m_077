6c12dd162a6331dc4ce3795cb6ca4cf2e110e0c8
firoz.nur <firoznur5@gmail.com>
2025-05-25 16:17:03 +0600

    Add : Daisy UI

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Meals from "./Meals";
+import styles from "./styles.module.css";
 
 const MealsPage = () => {
   return (
     <div className="p-12">
       <h1 className="text-3xl font-semibold text-red-400">Choose Your Meals</h1>
-      <p>Choose meals of your choice by searching</p>
+      <p
+        className={`${styles.font_tomato} ${styles.text_large} btn btn-primary`}>
+        Choose meals of your choice by searching
+      </p>
       <Meals></Meals>
     </div>
   );
